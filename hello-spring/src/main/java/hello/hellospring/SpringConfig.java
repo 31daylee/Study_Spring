56d189ce41b6b128f14bae51d80ed1ee3381cd91
gussla2206 <gussla2206@gmail.com>
2023-11-11 18:46:22 +0900

    25강 스프링데이터 JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -3,7 +3,6 @@ package hello.hellospring;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,24 +10,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+   /* private EntityManager em;
 
     // em 선언 후 생성자 만듦
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
-    }
+    }*/
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+ /*   @Bean
     public MemberRepository memberRepository(){
 
-      /*  //return new MemoryMemberRepository();
-        return new JdbcTempMemberRepository(dataSource);*/
-        return new JpaMemberRepository(em);
-    }
+      *//*  //return new MemoryMemberRepository();
+        return new JdbcTempMemberRepository(dataSource);
+        return new JpaMemberRepository(em);*//*
+
+
+    }*/
 }
